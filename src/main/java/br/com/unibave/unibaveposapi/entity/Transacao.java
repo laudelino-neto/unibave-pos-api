472343bb0180ddc1805949b93994eab625631630
Laudelino Neto <laudelino_sc@hotmail.com>
2022-11-18 14:07:29 -0300

    -Remoção de validação desnecessária

--- src/main/java/br/com/unibave/unibaveposapi/entity/Transacao.java
+++ src/main/java/br/com/unibave/unibaveposapi/entity/Transacao.java
@@ -11,7 +11,6 @@ import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Positive;
 import javax.validation.constraints.Size;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
